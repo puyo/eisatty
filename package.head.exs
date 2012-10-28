75b80df7727c2bf19eb3d2e36738f908278a0147
Yurii Rashkovskii <yrashk@gmail.com>
2012-10-28 15:51:41 -0700

    Add expm package

---
+++ package.head.exs
@@ -0,0 +1,6 @@
+Expm.Package.new(name: "eisatty", description: "isatty(STDOUT) for Erlang",
+                 version: :head, keywords: ["terminal","ansi","tty"], 
+                 maintainers: [[name: "Yurii Rashkovskii", 
+                                email: "yrashk@gmail.com"]],
+                 repositories: [[github: "yrashk/eisatty"]])
+    
